6169bc9bdbda370ec17844d75546c28e76d53125
torcheRR <berkayocer2003@gmail.com>
2022-08-18 18:07:59 +0300

    aa

--- Dr. Angela YU/Day to Day/Day3.9.py
+++ Dr. Angela YU/Day to Day/Day3.9.py
@@ -1,12 +1,12 @@
 print("Welcome to the Love Calculator!")
-name1=input("What is your name? \n")
-name2=input("What is their name? \n")
-lower_1=name1.lower
-onlarbasamagi=lower_1.count("t") + lower_1.count("r") + lower_1.count("u") + lower_1.count("e")
-lower_2=name2.lower
-birlerbasamagi=int(lower_2.count("l")) + int(lower_2.count("o")) + int(lower_2.count("v")) + int(lower_2.count("e"))
+name1=str(input("What is your name? \n"))
+name2=str(input("What is their name? \n"))
+lower_1=name1.lower()
+lower_2=name2.lower()
+onlarbasamagi=int(lower_1.count("t") + lower_1.count("r") + lower_1.count("u") + lower_1.count("e") +lower_2.count("t") + lower_2.count("r") + lower_2.count("u") + lower_2.count("e") )*10
+birlerbasamagi=int((lower_1.count("l")) + lower_1.count("o") + lower_1.count("v") + lower_1.count("e") + (lower_2.count("l")) + lower_2.count("o") + lower_2.count("v") + lower_2.count("e"))
 sonsayi=onlarbasamagi+birlerbasamagi
-if sonsayi<10 and sonsayi>90:
+if sonsayi<10 or sonsayi>90:
     print(f"Your score is {sonsayi}, you go together like coke and mentos.")
 elif sonsayi<=50 and sonsayi>=40:
     print(f"Your score is {sonsayi}, you are alright together.")
